3758e58676603dc46f6c85f36324f7f85e45ca59
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-08-03 22:09:43 +0530

    fix level 8

--- game/scenes/bodies/engine_sound.gd
+++ game/scenes/bodies/engine_sound.gd
@@ -4,4 +4,4 @@ var pitch_range: Vector2 = Vector2(0.2, 0.4)
 
 func _physics_process(delta: float) -> void:
 	pitch_scale = clampf(BaseCar.I.velocity.length() * delta * 0.1, pitch_range.x, pitch_range.y)
-	print(pitch_scale)
+	#print(pitch_scale)
