48ba8377991712607b61be33530096aa7f77138d
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-08-03 12:47:38 +0530

    save the changes, added a theme mainly

--- addons/my-ecosystem-template/transitions/scene_transition_manager.gd
+++ addons/my-ecosystem-template/transitions/scene_transition_manager.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 signal screen_covered
 
-#const CIRCLE_SHADER = preload("uid://dnms5hsmipkyv")
+const SHADER = preload("uid://cxnlsq7fn1k7y")
 
 @onready var transition_player: AnimationPlayer = $TransitionRect/TransitionPlayer
 @onready var transition_rect : ColorRect = $TransitionRect
@@ -13,41 +13,37 @@ signal screen_covered
 var single_transition : bool
 
 enum TransitionNames {
-	circleIn,
-	circleOut,
+	wipeToColor,
+	wipeToNormal,
 	fadeToBlack,
 	slightFlash,
 	fadeToNormal
 }
 
-const TRANSISTION_NAMES = {
-	fadeToBlack = "fadeToBlack",
-	slightFlash = "slightFlash",
-	circleOut = "circleOut",
-	circleIn = "circleIn",
-	fadeToNormal = "fadeToNormal"
-}
+static func get_enum_key_name(key: TransitionNames) -> StringName:
+	return TransitionNames.keys()[key]
 
 func _ready() -> void:
 	transition_rect.visible = false
 	white_rect.visible = false
 	shader_rect.visible = false
 
-func change_scene_with_transition(scene_path: String, anim_name: String = "fadeToBlack", pop_up: bool = false) -> void:
-	transition(anim_name)
+func change_scene_with_transition(scene_path: String, anim_key: TransitionNames = TransitionNames.fadeToBlack, pop_up: bool = false) -> void:
+	transition(anim_key)
 	await screen_covered
 	if !pop_up:
 			A.tree.change_scene_to_file(scene_path)
 
-func change_scene_with_transition_packed(scene: PackedScene, anim_name: String = "fadeToBlack") -> void:
-	transition(anim_name)
+func change_scene_with_transition_packed(scene: PackedScene, anim_key: TransitionNames = TransitionNames.fadeToBlack) -> void:
+	transition(anim_key)
 	await screen_covered
 	A.tree.change_scene_to_packed(scene)
 	
 
-func transition(anim_name: StringName = "fadeToBlack", single_transition_only: bool = false, speed_scale: float = 1, pop_up: bool = false) -> void:
+func transition(anim_key: TransitionNames = TransitionNames.fadeToBlack, single_transition_only: bool = false, speed_scale: float = 1, pop_up: bool = false) -> void:
 	if single_transition_only:
 			single_transition = true
+	var anim_name := get_enum_key_name(anim_key)
 	match anim_name:
 			"fadeToBlack":
 					transition_player.speed_scale = speed_scale
@@ -58,15 +54,15 @@ func transition(anim_name: StringName = "fadeToBlack", single_transition_only: b
 					transition_player.speed_scale = speed_scale
 					white_rect.visible = true
 					transition_player.play(anim_name)
-			"circleIn":
+			"wipeToColor":
 					transition_player.speed_scale = speed_scale
-					#transition_rect.material.shader = CIRCLE_SHADER
+					#transition_rect.material.shader = SHADER
 					transition_rect.modulate = Color.WHITE
 					transition_rect.visible = true
 					transition_player.play(anim_name)
-			"circleOut":
+			"wipeToNormal":
 					transition_player.speed_scale = speed_scale
-					#transition_rect.material.shader = CIRCLE_SHADER
+					#transition_rect.material.shader = SHADER
 					transition_rect.visible = true
 					transition_rect.modulate = Color.WHITE
 					transition_player.play(anim_name)
@@ -85,10 +81,10 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 						transition_rect.hide()
 				"slightFlash":
 						white_rect.hide()
-				"circleIn":
+				"wipeToColor":
 						screen_covered.emit()
-						transition_player.play("circleOut")
-				"circleOut":
+						transition_player.play("wipeToNormal")
+				"wipeToNormal":
 						transition_rect.hide()
 	single_transition = false
 
--- game/scripts/save_data.gd
+++ game/scripts/save_data.gd
@@ -1,3 +1,15 @@
 class_name SaveData extends SaveManager
 
-static var LevelData : Dictionary[StringName, float] #Level_name, Time
+static var level_data : Dictionary[StringName, Variant] #Level_name, Time
+
+static var global_data : Dictionary
+
+static func reset():
+	global_data = {
+		prev_level_index = 1
+	}
+	level_data = {
+		best_time = 0.0, #in ms
+		completed = false
+	}
+	
